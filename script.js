033209f524c0f97db584f3e030bdde738cfec97a
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2024-03-03 12:53:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const p = document.querySelector("p");
+const button = document.querySelector("button");
+button.addEventListener("click",()=>{
+	p.innerText="";
+	const newEle = document.createElement("h1");
+	newEle.innerText="Entered Metaverse";
+	p.appendChild(newEle);
+})
